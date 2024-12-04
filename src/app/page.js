2f1b56f3452288629ee38c81b10559b48dfd7fd0
Giovanna Borges <grborgesp@gmail.com>
2024-12-04 19:36:13 -0300

    config modal

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-import {CheckboxGroup, Checkbox} from "@nextui-org/react";
-import {Button, ButtonGroup} from "@nextui-org/button";
+import { CheckboxGroup, Checkbox } from "@nextui-org/react"
+import NovaAgenda from "../components/NovaAgenda"
+
 
 export default function Home() {
   return (
@@ -11,15 +12,13 @@ export default function Home() {
         <main>
           <div className="flex-col-reverse h-screen w-1/4 justify-center items-center">
             <h1 className="text-3xl">Agendas</h1>
-              <CheckboxGroup defaultValue={["agenda1", "agenda2"]}>
-                <Checkbox value="agenda1">Agenda 1</Checkbox>
-                <Checkbox value="agenda2">Agenda 2</Checkbox>
-                <Checkbox value="agenda3">Agenda 3</Checkbox>
-                <Checkbox value="agenda4">Agenda 4</Checkbox>
-              </CheckboxGroup>
-              <Button size="md" variant="ghost" radius="full" className="bg-gradient-to-tr from-teal-500 to-cyan-500 text-white shadow-lg">
-                Nova Agenda
-              </Button>
+            <CheckboxGroup defaultValue={["agenda1", "agenda2"]}>
+              <Checkbox value="agenda1">Agenda 1</Checkbox>
+              <Checkbox value="agenda2">Agenda 2</Checkbox>
+              <Checkbox value="agenda3">Agenda 3</Checkbox>
+              <Checkbox value="agenda4">Agenda 4</Checkbox>
+            </CheckboxGroup>
+            <NovaAgenda />
           </div>
         </main>
         <Footer />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
-const {nextui} = require('@nextui-org/theme');
+// Instalação das configurações do Tailwind do NextUI
+const {nextui} = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    // ...
+    // make sure it's pointing to the ROOT node_module
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/components/(calendar|checkbox|button|ripple|spinner).js"
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
+  darkMode: "class",
   plugins: [nextui()],
 };
